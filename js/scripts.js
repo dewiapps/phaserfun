a2bbf9751af2a9cd48f11507c44735423ff8cf65
Nate <natedeanmail@gmail.com>
2014-10-20 15:02:20 -0500

    Added audio

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ var text = "";
 var counter = 0;
 var notes;
 var currChord;
+var fx;
 
 var frets = [100,150,200,250,300,350,400,450,500];
 
@@ -21,14 +22,24 @@ var questions = {
 
 function preload(){
   game.load.image('note', 'images/bullet.png');
+  game.load.audio('sfx', [ 'audio/fx_mixdown.mp3','audio/fx_mixdown.ogg' ]);
 }
 
 function create(){
   text = game.add.text(32, 380, '', { font: "30pt Courier", fill: "#19cb65", stroke: "#119f4e", strokeThickness: 2 });
   
+    fx = game.add.audio('sfx');
+    fx.allowMultiple = true;
+	fx.addMarker('alien death', 1, 1.0);
+	fx.addMarker('boss hit', 3, 0.5);
+	fx.addMarker('escape', 4, 3.2);
+	fx.addMarker('meow', 8, 0.5);
+	fx.addMarker('numkey', 9, 0.1);
+	fx.addMarker('ping', 10, 1.0);
+	fx.addMarker('death', 12, 4.2);
+	fx.addMarker('shot', 17, 1.0);
+	fx.addMarker('squit', 19, 0.3);
   
-
-    
 }
 
 function update(){
@@ -58,10 +69,15 @@ function updateText(){
 }
 
 function setNotes(){
+    
     var currNote = currChord.notes[counter];
     if (currNote < 0 ){
+      // this is an X
+      fx.play('squit');
       var note = notes.create(counter*70 + 10, 50, 'note', 0);
-    }else{
+    }else{ 
+      // not an X
+      fx.play('ping');
       var note = notes.create(counter*70 + 10, frets[currChord.notes[counter]], 'note', 0);
     }
     
