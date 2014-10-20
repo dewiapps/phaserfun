e1b9759dda16225cd7cdbd97882295a1a2d69e35
Nate <natedeanmail@gmail.com>
2014-10-20 14:24:02 -0500

    Initial commit for phaser project2

--- js/scripts.js
+++ js/scripts.js
@@ -1,132 +1,69 @@
-var game = new Phaser.Game(500,500, Phaser.AUTO, 'game',{preload: preload, create: create, update: update});
+var game = new Phaser.Game(500,500, Phaser.CANVAS, 'game',{preload: preload, create: create, update: update});
 
-var background;
+var text = "";
+var counter = 0;
+var notes;
+var currChord;
 
-var land;
+var frets = [100,150,200,250,300,350,400,450,500];
 
-var shadow;
-var tank;
-var turret;
 
-var enemies;
-var enemyBullets;
-var enemiesTotal = 0;
-var enemiesAlive = 0;
-var explosions;
-
-var logo;
-
-var currentSpeed = 0;
-var cursors;
-
-var bullets;
-var fireRate = 100;
-var nextFire = 0;
+var questions = {
+  easy: [
+    {name: "G", notes: [3,2,0,0,3,3]},
+    {name: "D", notes: [-1,-1,0,2,3,2]},
+    {name: "C", notes: [-1,3,2,0,1,0]}
+  ],
+  medium: ["Gadd9","Csus","D7"],
+  hard: ["G11","C9","Dmin7b5"]
+  
+} // end questions
 
 function preload(){
-  game.load.image('starfield', 'images/starfield.jpg');
-  game.load.atlas('tank', 'images/tanks.png', 'images/tanks.json');
-  game.load.image('bullet', 'images/bullet.png');
-  game.load.spritesheet('kaboom', 'images/explosion.png', 64, 64, 23);
+  game.load.image('note', 'images/bullet.png');
 }
 
 function create(){
-  background = game.add.sprite(0, 0, 'starfield');
+  text = game.add.text(32, 380, '', { font: "30pt Courier", fill: "#19cb65", stroke: "#119f4e", strokeThickness: 2 });
   
-  tank = game.add.sprite(0, 0, 'tank', 'tank1');
-  tank.anchor.setTo(0.5, 0.5);
-  tank.animations.add('move', ['tank1', 'tank2', 'tank3', 'tank4', 'tank5', 'tank6'], 20, true);
-  //  This will force it to decelerate and limit its speed
-  game.physics.enable(tank, Phaser.Physics.ARCADE);
-  tank.body.drag.set(0.2);
-  tank.body.maxVelocity.setTo(400, 400);
-  tank.body.collideWorldBounds = true;
   
-  //  Finally the turret that we place on-top of the tank body
-  turret = game.add.sprite(0, 0, 'tank', 'turret');
-  turret.anchor.setTo(0.3, 0.5);
-  
-  //  Our bullet group
-    bullets = game.add.group();
-    bullets.enableBody = true;
-    bullets.physicsBodyType = Phaser.Physics.ARCADE;
-    bullets.createMultiple(30, 'bullet', 0, false);
-    bullets.setAll('anchor.x', 0.5);
-    bullets.setAll('anchor.y', 0.5);
-    bullets.setAll('outOfBoundsKill', true);
-    bullets.setAll('checkWorldBounds', true);
 
-    //  Explosion pool
-    explosions = game.add.group();
-
-    for (var i = 0; i < 10; i++)
-    {
-        var explosionAnimation = explosions.create(0, 0, 'kaboom', [0], false);
-        explosionAnimation.anchor.setTo(0.5, 0.5);
-        explosionAnimation.animations.add('kaboom');
-    }
-
-    tank.bringToTop();
-    turret.bringToTop();
-
-    cursors = game.input.keyboard.createCursorKeys();
+    
 }
 
 function update(){
   
-  if (cursors.left.isDown)
-    {
-        tank.angle -= 4;
-    }
-    else if (cursors.right.isDown)
-    {
-        tank.angle += 4;
-    }
-
-    if (cursors.up.isDown)
-    {
-        //  The speed we'll travel at
-        currentSpeed = 300;
-    }
-    else
-    {
-        if (currentSpeed > 0)
-        {
-            currentSpeed -= 4;
-        }
-    }
-
-    if (currentSpeed > 0)
-    {
-        game.physics.arcade.velocityFromRotation(tank.rotation, currentSpeed, tank.body.velocity);
-    }
-
-
-    turret.x = tank.x;
-    turret.y = tank.y;
-
-    turret.rotation = game.physics.arcade.angleToPointer(turret);
-
-    if (game.input.activePointer.isDown)
-    {
-        //  Boom!
-        fire();
-    }
+  game.input.onDown.addOnce(updateText, this);
+  
+  
 
 }// end update
 
-function fire () {
 
-    if (game.time.now > nextFire && bullets.countDead() > 0)
-    {
-        nextFire = game.time.now + fireRate;
-
-        var bullet = bullets.getFirstExists(false);
-
-        bullet.reset(turret.x, turret.y);
-
-        bullet.rotation = game.physics.arcade.moveToPointer(bullet, 1000, game.input.activePointer, 500);
-    }
-
-}
+function updateText(){
+  counter = 0;
   
+  if(notes){
+    notes.destroy();
+  }
+  
+  notes = game.add.group();
+
+  var rand = game.rnd.integerInRange(0, questions.easy.length);
+  currChord = questions.easy[rand];
+  
+  game.time.events.repeat(300, 6, setNotes, this);
+  
+  text.setText(currChord.name);
+}
+
+function setNotes(){
+    var currNote = currChord.notes[counter];
+    if (currNote < 0 ){
+      var note = notes.create(counter*70 + 10, 50, 'note', 0);
+    }else{
+      var note = notes.create(counter*70 + 10, frets[currChord.notes[counter]], 'note', 0);
+    }
+    
+    counter++;
+}
